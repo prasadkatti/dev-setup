0cb64e93645c627627213eb48a2ef2e7b4fbc36b
Prasad Katti <prasadmkatti@gmail.com>
2017-06-23 20:56:24 -0700

    Install jq and add git br to prompt

--- provision_scripts/setup_machine.sh
+++ provision_scripts/setup_machine.sh
@@ -9,4 +9,15 @@ echo 'source /usr/share/virtualenvwrapper/virtualenvwrapper.sh' >> /home/ubuntu/
 apt-get install -q -y libssl-dev libffi-dev
 pip install ansible
 
-echo 'alias cl=clear' >> /home/ubuntu/.bashrc
+apt-get install -q -y jq
+
+cat <<EOT >> /home/ubuntu/.bashrc
+alias cl=clear
+
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+
+EOT
